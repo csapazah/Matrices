96428c5d5fa1be88e3a1149384c40dacf14936b6
csapazah <csapazah@unjbg.edu.pe>
2026-07-09 16:24:37 -0500

    Dimensiones de matrices y verificacion de la condicion

--- Ejercicios_Matrices/Producto_de_dos_matrices.cpp
+++ Ejercicios_Matrices/Producto_de_dos_matrices.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 int main(){
     int m,n,a,b,A[100][100],B[100][100],P[100][100];
+    cout << "Dimensiones de la primera matriz" << endl;
+    cin >> m >> n;
+    cout << "Dimensiones de la segunda matriz" << endl;
+    cin >> a >> b;
+    
+    if (n!=a){
+    	cout << "Error";
+    	return 1;
+	}
     return 0;
 }
