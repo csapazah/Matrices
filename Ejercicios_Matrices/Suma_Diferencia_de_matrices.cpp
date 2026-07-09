433c673e51b8a271be69c8bfd1d82e26851f1726
csapazah <csapazah@unjbg.edu.pe>
2026-07-09 15:03:43 -0500

    Lectura de cada matriz

--- Ejercicios_Matrices/Suma_Diferencia_de_matrices.cpp
+++ Ejercicios_Matrices/Suma_Diferencia_de_matrices.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main(){
     int m,n,A[100][100],B[100][100];
+    cin >> m >> n;
+	cout << "\n Primera matriz"<< endl;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			cin >> A[i][j];
+		}
+	}
+	cout << "\n Segunda matriz" << endl;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			cin >> B[i][j];
+		}
+	}
     return 0;
 }
