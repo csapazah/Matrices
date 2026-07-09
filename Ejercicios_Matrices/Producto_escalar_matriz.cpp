233b6cf0934b2018da197b1dcc1537e05db88797
csapazah <csapazah@unjbg.edu.pe>
2026-07-09 15:21:50 -0500

    Lectura de la matriz

--- Ejercicios_Matrices/Producto_escalar_matriz.cpp
+++ Ejercicios_Matrices/Producto_escalar_matriz.cpp
@@ -3,5 +3,11 @@ using namespace std;
 int main(){
     int m,n,A[100][100];
 	float K;
+    cin >> m >> n;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			cin >> A[i][j];
+		}
+	}
     return 0;
 }
