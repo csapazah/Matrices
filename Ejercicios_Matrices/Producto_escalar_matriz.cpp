5b0ec1003738c2602c926458c0a232f81b732dd5
csapazah <csapazah@unjbg.edu.pe>
2026-07-09 15:22:54 -0500

    Salida del producto escalar de la matriz

--- Ejercicios_Matrices/Producto_escalar_matriz.cpp
+++ Ejercicios_Matrices/Producto_escalar_matriz.cpp
@@ -9,5 +9,13 @@ int main(){
 			cin >> A[i][j];
 		}
 	}
+    cout << endl;
+	cin >> K;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			cout << A[i][j]*K << " ";
+		}
+		cout << endl;
+	}
     return 0;
 }
